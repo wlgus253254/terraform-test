53d4b05c3ca89d8f686cc07bb48e8fed46b37e0c
wlgus253254 <ansible-user@ip-172-16-0-100.ap-northeast-2.compute.internal>
2024-02-01 08:43:37 +0000

    module

---
+++ stage/services/webserver-cluster/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+module "webserver_cluster" {
+  source = "/home/ansible-user/new_basic/module/services/webserver-cluster"
+  cluster_name = "webservers-stage"
+  db_remote_state_bucket = "terraform-state-cloudwave-07"
+  db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
+  instance_type = "t3.micro"
+  min_size = 2
+  max_size = 2
+}
+
+resource "aws_security_group_rule" "allow_testing_inbound" {
+  type = "ingress"
+  security_group_id = module.webserver_cluster.alb_security_group_id
+  from_port = 12345
+  to_port = 12345
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
